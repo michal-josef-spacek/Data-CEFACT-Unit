5216dae6475de6f43dd72ddffdcc62c0f266608a
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-07-08 23:31:53 +0200

    Add doc

--- Unit.pm
+++ Unit.pm
@@ -60,3 +60,218 @@ sub BUILD {
 }
 
 1;
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Data::CEFACT::Unit - Data object for CEFACT unit.
+
+=head1 SYNOPSIS
+
+ use Data::CEFACT::Unit;
+
+ my $obj = Data::CEFACT::Unit->new(%params);
+ my $common_code = $obj->common_code;
+ my $conversion_factor = $obj->conversion_factor;
+ my $description = $obj->description;
+ my $level_category = $obj->level_category;
+ my $name = $obj->name;
+ my $status = $obj->status;
+ my $symbol = $obj->symbol;
+
+=head1 METHODS
+
+=head2 C<new>
+
+ my $obj = Data::CEFACT::Unit->new(%params);
+
+Constructor.
+
+=over 8
+
+=item * C<common_code>
+
+Common code of unit.
+
+It's required.
+
+Default value is undef.
+
+=item * C<conversion_factor>
+
+Conversion factor of unit.
+
+It's optional.
+
+Default value is undef.
+
+=item * C<description>
+
+Unit description.
+
+It's optional.
+
+Default value is undef.
+
+=item * C<level_category>
+
+Unit level/category.
+
+It's required.
+
+Default value is undef.
+
+=item * C<name>
+
+Unit name.
+
+It's required.
+
+Default value is undef.
+
+=item * C<status>
+
+Unit status,
+
+Possible statuses are undef as valid, 'D' as deprecated, 'X' as invalid and '¦'
+as new.
+
+It's optional, default value is valid status.
+
+Default value is undef.
+
+=item * C<symbol>
+
+Unit symbol.
+
+It's optional.
+
+Default value is undef.
+
+=back
+
+Returns instance of object.
+
+=head2 C<common_code>
+
+ my $common_code = $obj->common_code;
+
+Get unit common code.
+
+Returns string.
+
+=head2 C<conversion_factor>
+
+ my $conversion_factor = $obj->conversion_factor;
+
+Get unit conversion factor.
+
+Returns string.
+
+=head2 C<description>
+
+ my $description = $obj->description;
+
+Get unit description.
+
+Returns string.
+
+=head2 C<level_category>
+
+ my $level_category = $obj->level_category;
+
+Get unit level/category.
+
+Returns string.
+
+=head2 C<name>
+
+ my $name = $obj->name;
+
+Get unit name.
+
+Returns string.
+
+=head2 C<status>
+
+ my $status = $obj->status;
+
+Get unit status.
+
+Returns string or undef.
+
+=head2 C<symbol>
+
+ my $symbol = $obj->symbol;
+
+Get unit symbol.
+
+Returns string.
+
+=head1 EXAMPLE
+
+=for comment filename=create_and_print_unit_kilogram.pl
+
+ use strict;
+ use warnings;
+
+ use Data::CEFACT::Unit;
+
+ my $obj = Data::CEFACT::Unit->new(
+         'common_code' => 'KGM',
+         'conversion_factor' => 'kg',
+         'description' => 'A unit of mass equal to one thousand grams.',
+         'level_category' => 1,
+         'name' => 'kilogram',
+         'symbol' => 'kg',
+ );
+
+ # Print out.
+ print 'Name: '.$obj->name."\n";
+ print 'Description: '.$obj->description."\n";
+ print 'Common code: '.$obj->common_code."\n";
+ print 'Status: '.(! defined $obj->status ? 'valid' : $obj->status)."\n";
+ print 'Symbol: '.$obj->symbol."\n";
+ print 'Level/Category: '.$obj->level_category."\n";
+ print 'Conversion factor: '.$obj->conversion_factor."\n";
+
+ # Output:
+ # Name: kilogram
+ # Description: A unit of mass equal to one thousand grams.
+ # Common code: KGM
+ # Status: valid
+ # Symbol: kg
+ # Level/Category: 1
+ # Conversion factor: kg
+
+=head1 DEPENDENCIES
+
+L<Mo>,
+L<Mo::utils>.
+L<Readonly>,
+L<Unicode::UTF8>.
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Data-CEFACT-Unit>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2025 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
